d820ecfd62dbd8a50564be562323dc9c258acbf9
dipreez <calloffate@naver.com>
2022-01-13 23:31:36 +0900

    2022.01.13

--- project/save_similarity.py
+++ project/save_similarity.py
@@ -6,6 +6,7 @@ from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 from scipy import sparse
 from tqdm import tqdm
+import sys
 
 def save_similarity():
     ################################################
@@ -37,9 +38,14 @@ def save_corrcoef():
     # output: saves correlation coefficient to corrcoef.npy
     ################################################
     bg_rating = pd.read_csv('./archive/bgg-15m-reviews.csv')
+
+    # randomly drop rows(due to memory lacking issue)
+    drop_indices = np.random.choice(bg_rating.index, int(bg_rating.shape[0]/2), replace=False)
+
+    bg_rating.drop(drop_indices, inplace=True)
     bg_rating.drop('comment', axis=1, inplace=True)
 
-    chunk_size = 1000000
+    chunk_size = 500000
     chunks = [x for x in range(0, bg_rating.shape[0], chunk_size)]
     chunks.append(bg_rating.shape[0])
     print(chunks)
@@ -50,16 +56,21 @@ def save_corrcoef():
                        .sum()
                        .unstack()
                        .reset_index()
-                       .fillna(0)
                        .set_index('user')
                        )
         pivot_chunk = pivot_chunk.astype(np.float16)
-        bg_rating_pivot = bg_rating_pivot.append(pivot_chunk, sort=False)
+        if bg_rating_pivot.empty:
+            bg_rating_pivot = bg_rating_pivot.append(pivot_chunk, sort=False)
+            print('hi')
+        else:
+            bg_rating_pivot = pd.merge(bg_rating_pivot, pivot_chunk, on='user', how='outer')
+        # bg_rating_pivot = bg_rating_pivot.groupby('user').sum().reset_index().set_index('user')
+        # print(np.nonzero(bg_rating_pivot.columns.duplicated()))
 
     bg_rating_sparse = sparse.csr_matrix(bg_rating_pivot.fillna(0).to_numpy())
     print(bg_rating_sparse)
     np.save('bg_rating_sparse.npy', bg_rating_sparse)
 
 if __name__ == '__main__':
-    save_similarity()
+    # save_similarity()
     save_corrcoef()
