e40fc1fa6cb1da64a86fef56b4ac417fefaff793
dipreez <calloffate@naver.com>
2022-01-20 22:20:33 +0900

    2022.01.20 commit

--- project/get_recommendation.py
+++ project/get_recommendation.py
@@ -4,10 +4,10 @@ from ast import literal_eval
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 
-def get_k_recommendation(board_game_title, k=10):
+def get_content_based_reccomendation(board_game_title, k=10):
     ################################################
     # input: gets title of a board game, k value
-    # output: returns k board games that are similar to the input board game
+    # output: returns list of top k board games that are similar to the input board game
     ################################################
     bg_info = pd.read_csv('./archive/games_detailed_info.csv')
     bg_info = bg_info[['primary', 'minplayers', 'maxplayers', 'boardgamecategory', 'boardgamemechanic']]
@@ -16,16 +16,39 @@ def get_k_recommendation(board_game_title, k=10):
 
     target_idx = bg_info[bg_info['primary'] == board_game_title].index.values
 
-    similar_idx = similarity[target_idx, :k].reshape(-1)
+    similar_idx = similarity[target_idx, :k+1].reshape(-1)
 
     similar_idx = similar_idx[similar_idx != target_idx]
 
     recommendation = bg_info.iloc[similar_idx]
+    recommendation = recommendation['primary'].tolist()
 
     print(recommendation)
 
     return recommendation
 
+def get_colaborative_filtering_recommendation(board_game_title, k=10):
+    ################################################
+    # input: gets title of a board game, k value
+    # output: returns list of top k board games that are similar to the input board game
+    ################################################
+    bg_titles = np.load('./bg_titles.npy')
+    corr_matrix = np.load('./corr_matrix.npy')
+    corr_matrix = corr_matrix.argsort()[:, ::-1]
+    bg_titles_list = list(bg_titles)
+
+    target_idx = bg_titles_list.index(board_game_title)
+
+    similar_idx = corr_matrix[target_idx, :k+1].reshape(-1)
+
+    similar_idx = similar_idx[similar_idx != target_idx]
+
+    recommendation = list(bg_titles[similar_idx])
+
+    print(recommendation)
+
+    return recommendation
 
 if __name__ == '__main__':
-    get_k_recommendation("Pandemic")
+    get_content_based_reccomendation("Pandemic")
+    get_colaborative_filtering_recommendation("Pandemic")
--- project/save_similarity.py
+++ project/save_similarity.py
@@ -4,11 +4,12 @@ from ast import literal_eval
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
+from sklearn.decomposition import TruncatedSVD
 from scipy import sparse
 from tqdm import tqdm
 import sys
 
-def save_similarity():
+def save_content_based_similarity():
     ################################################
     # input:
     # output: saves similarity matrix calculated by content-based filtering to similarity.npy
@@ -32,25 +33,23 @@ def save_similarity():
 
     np.save('similarity.npy', similarity)
 
-def save_corrcoef():
+def save_corr_matrix():
     ################################################
     # input:
-    # output: saves correlation coefficient to corrcoef.npy
+    # output: saves correlation matrix to corr_matrix.npy
     ################################################
     bg_rating = pd.read_csv('./archive/bgg-15m-reviews.csv')
 
-    # randomly drop rows(due to memory lacking issue)
-    drop_indices = np.random.choice(bg_rating.index, int(bg_rating.shape[0]/2), replace=False)
+    # drop rows(due to memory lacking issue)
+    bg_rating = bg_rating[:-int(bg_rating.shape[0]/4)]
 
-    bg_rating.drop(drop_indices, inplace=True)
     bg_rating.drop('comment', axis=1, inplace=True)
 
     chunk_size = 500000
     chunks = [x for x in range(0, bg_rating.shape[0], chunk_size)]
     chunks.append(bg_rating.shape[0])
-    print(chunks)
     bg_rating_pivot = pd.DataFrame(dtype=np.float16)
-    for i in tqdm(range(0, len(chunks))):
+    for i in tqdm(range(0, len(chunks)-1)):
         bg_rating_chunk = bg_rating.iloc[chunks[i]:chunks[i+1] - 1]
         pivot_chunk = (bg_rating_chunk.groupby(['user', 'name'])['rating']
                        .sum()
@@ -59,18 +58,28 @@ def save_corrcoef():
                        .set_index('user')
                        )
         pivot_chunk = pivot_chunk.astype(np.float16)
-        if bg_rating_pivot.empty:
-            bg_rating_pivot = bg_rating_pivot.append(pivot_chunk, sort=False)
-            print('hi')
-        else:
-            bg_rating_pivot = pd.merge(bg_rating_pivot, pivot_chunk, on='user', how='outer')
-        # bg_rating_pivot = bg_rating_pivot.groupby('user').sum().reset_index().set_index('user')
+        bg_rating_pivot = bg_rating_pivot.append(pivot_chunk, sort=False)
+        # if bg_rating_pivot.empty:
+        #     bg_rating_pivot = bg_rating_pivot.append(pivot_chunk, sort=False)
+        #     print('hi')
+        # else:
+        #     bg_rating_pivot = pd.merge(bg_rating_pivot, pivot_chunk, on='user', how='outer')
+        bg_rating_pivot = bg_rating_pivot.groupby('user').sum().reset_index().set_index('user')
         # print(np.nonzero(bg_rating_pivot.columns.duplicated()))
 
-    bg_rating_sparse = sparse.csr_matrix(bg_rating_pivot.fillna(0).to_numpy())
-    print(bg_rating_sparse)
-    np.save('bg_rating_sparse.npy', bg_rating_sparse)
+    # bg_rating_sparse = sparse.csr_matrix(bg_rating_pivot.fillna(0).to_numpy())
+    bg_user_matrix = bg_rating_pivot.T
+    SVD = TruncatedSVD(n_components=20)
+    corrcoef_matrix = SVD.fit_transform(bg_user_matrix)
+    corr_matrix = np.corrcoef(corrcoef_matrix)
+
+    title = bg_rating_pivot.columns
+    title_array = np.array(list(title))
+
+    np.save('corr_matrix.npy', corr_matrix)
+    np.save('bg_titles.npy', title_array)
+
 
 if __name__ == '__main__':
-    # save_similarity()
-    save_corrcoef()
+    save_content_based_similarity()
+    save_corr_matrix()
