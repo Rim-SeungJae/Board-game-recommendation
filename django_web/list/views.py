1070efd14184bad75ddb83b41c2ed164244067d8
Rim-SeungJae <calloffate@naver.com>
2024-09-27 18:54:35 +0900

    fix errors on ncf matrix load 2

--- django_web/list/views.py
+++ django_web/list/views.py
@@ -92,7 +92,7 @@ bg_titles = np.load(bg_titles_path)
 similarity = np.load(similarity_path)
 # corr_matrix = np.load(corr_matrix_path)
 # corr_matrix = corr_matrix.argsort()[:, ::-1]
-ncf_corr_matrix = np.load(ncf_corr_matrix_path, allow_pickle=True)
+ncf_corr_matrix = np.load(ncf_corr_matrix_path, allow_pickle=True).item()
 
 mapping_table_path = os.path.join(settings.MEDIA_ROOT, 'npys', 'map.npy')
 mapping_table = np.load(mapping_table_path, allow_pickle=True)
